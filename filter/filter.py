b4ee99246509e12cbd5cbd4580750f1bc3951569
Terranlee <ltrthu@163.com>
2016-01-11 23:37:20 +0800

    filter and directories

---
+++ filter/filter.py
@@ -0,0 +1,16 @@
+# Filter for xuetangX log data
+
+class Filter(object):
+    '''docstring for Filter
+        This is a filter for log data from xuetangX.
+        Use this to get all the data from a specific course.
+    '''
+
+    def __init__(self):
+        super(Filter, self).__init__()
+
+def main():
+
+if __name__ == '__main__':
+    main()
+
