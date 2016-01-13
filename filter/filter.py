dcada532d331a1c58c357f7cea983ff7362dff3a
Terranlee <ltrthu@163.com>
2016-01-14 00:50:00 +0800

    update video filter

--- filter/filter.py
+++ filter/filter.py
@@ -7,7 +7,7 @@ import gzip
 class Filter(object):
     '''docstring for Filter
         This is a filter for log data from xuetangX.
-        Use this to get all the data from a specific course.
+        Use this to get all the data from a specific course, and do some basic statistics.
     '''
 
     def __init__(self, s_date, e_date, cid):
@@ -47,16 +47,8 @@ class Filter(object):
             try:
                 content = json.loads(i, strict=False)
                 cid = content['context']['course_id']
-                # only record log data from the browser, not server
+                # in this step we just filter all the log data related to course_id
                 if cid.find(self.c_id) != -1:
-                #if cid.find(self.c_id) != -1 and content['event_source'] == 'browser':
-                    #newlog = dict()
-                    #newlog['event_type'] = content['event_type']
-                    #newlog['event'] = content['event']
-                    #newlog['context'] = content['context']
-                    #newlog['referer'] = content['referer']
-                    #newlog['time'] = content['time']
-                    #self.output.write(json.dumps(newlog) + '\n')
                     self.output.write(i + '\n')
                     cid_counter += 1
                 valid_counter += 1
@@ -87,15 +79,31 @@ class Filter(object):
 
     def __parse_log_by_event_type_sub(self, filename):
         print ('parse' + filename)
+        counter = 0
         video_counter = 0
         analytic_counter = 0
         invalid_counter = 0
         for i in open(filename):
             try:
+                counter += 1
+                if counter % 100000 == 0:
+                    print (counter)
+
                 content = json.loads(i, strict=False)
                 event_type = content['event_type']
+                # all the logs related to video
+                # remove those without username or user_id
+                # only need to save part of the original json data
                 if event_type in self.video_type:
-                    self.video_out.write(i + '\n')
+                    if content['username'] == '' or content['context']['user_id'] == '':
+                        continue
+                    newlog = {
+                        'event_type' : content['event_type'],
+                        'event' : content['event'],
+                        'context' : content['context'],
+                        'time' : content['time']
+                    }
+                    self.video_out.write(json.dumps(newlog) + '\n')
                     video_counter += 1
                 elif event_type in self.analytic_type:
                     self.analytic_out.write(i + '\n')
@@ -112,7 +120,7 @@ class Filter(object):
         self.video_out = '../result/' + self.c_id + '.video'
         self.analytic_out = '../result/' + self.c_id + '.analytic'
 
-        self.video_type = {'play_video', 'pause_video'}
+        self.video_type = {'play_video', 'pause_video', 'seek_video', 'load_video_error'}
         self.analytic_type = {'/analytic_track/i', '/analytic_track/p', '/analytic_track/t'}
 
         video_counter = 0
@@ -125,6 +133,12 @@ class Filter(object):
         self.video_out.close()
         self.analytic_out.close()
 
+    def calc_list_sum(self, timelist):
+        sum_time = overlap_time = 0
+        timelist.sort()
+        temp = [i[1] - i[0] for i in timelist]
+        sum_time = sum(temp)
+
     def parse_video_time_by_user(self):
         filename = '../result/' + self.c_id + '.video'
         invalid_counter = 0
@@ -132,25 +146,67 @@ class Filter(object):
         # value of this dict is the start time of this video
         # when we meet the pause_video, we aggregate the time of this video
         user_video_time = dict()
-        for i in filename:
+        for i in open(filename):
             try:
                 content = json.loads(i, strict=False)
-                uname = content['username']
                 uid = content['context']['user_id']
-                if uname == '' or uid == '':
-                    invalid_counter += 1
-                    continue
-
                 if uid not in user_video_time:
                     user_video_time[uid] = {}
-
                 date = content['time'][:10]
                 if date not in user_video_time[uid]:
                     user_video_time[uid][date] = {}
-                
+                vid = content['event']['id']
+                if vid not in user_video_time[uid][date]:
+                    user_video_time[uid][date][vid] = [ [-1, -1] ]
+
+                timelist = user_video_time[uid][date][vid]
+                etype = content['event_type']
+                if etype == 'play_video':
+                    if timelist[-1][0] == -1:
+                        timelist[-1][0] = content['event']['currentTime']
+                if etype == 'pause_video':
+                    if timelist[-1][0] != -1:
+                        timelist[-1][1] = content['event']['currentTime']
+                        timelist.append( [-1, -1] )
+                if etype == 'seek_video':
+                    # seek when the video is playing
+                    # if seek when the video is paused, do nothing
+                    if timelist[-1][0] != -1:
+                        timelist[-1][1] = content['event']['old_time']
+                        timelist.append( [content['event']['new_time'], -1] )
+                if etype == 'load_video_error':
+                    if timelist[-1][0] != 0:
+                        timelist[-1][1] = content['event']['currentTime']
+                        timelist.append( [-1, -1] )
             except (ValueError, KeyError):
                 invalid_counter += 1
                 continue
+
+        # check if the list is correct
+        # pop the last one if it is not finished
+        invalid_counter = 0
+        invalid_video = open('../result/' + self.c_id + '.ivid', 'w')
+        for user in user_video_time:
+            for date in user_video_time[user]:
+                for video in user_video_time[user][date]:
+                    timelist = user_video_time[user][date][video]
+                    if timelist[-1][1] == -1:
+                        timelist.pop()
+                    for i in timelist:
+                        if i[1] < i[0]:
+                            invalid_counter += 1
+                            invalid_video.write('%s %s %s : ' % (user, date, video))
+                            invalid_video.write(repr(timelist) + '\n')
+                            del( user_video_time[user][date][video] )
+        print ('Total %d invalid video' % (invalid_counter))
+        invalid_video.close()
+
+        # sort and compute video time
+        for user in user_video_time:
+            for date in user_video_time[user]:
+                for video in user_video_time[user][date]:
+                    timelist = user_video_time[user][date][video]
+                    sum_time, overlap_time = self.calc_list_sum(timelist)
 
     def test(self):
         '''
@@ -183,9 +239,9 @@ class Filter(object):
 
 def main():
     f = Filter(20150906, 20151231, '20740042X')
-    f.gen_gzfilelist()
-    f.parse_gzfile_cid()
-    #f.test()
+    #f.gen_gzfilelist()
+    #f.parse_gzfile_cid()
+    f.test()
 
 if __name__ == '__main__':
     main()
