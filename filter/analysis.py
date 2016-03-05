b3d437fb89657a5a189a48f175793556138d8c63
Terranlee <ltrthu@163.com>
2016-03-05 17:09:02 +0800

    update code: add param, need to decide later

--- filter/analysis.py
+++ filter/analysis.py
@@ -11,7 +11,32 @@ class Analyzer(object):
         super(Analyzer, self).__init__()
         self.result_dir = '../result/'
         self.c_id = cid
+        self.event_param = {
+            'watch_video': 0.0,
+            'django_comment_client.base.views.vote_for_thread': 0.0,
+            'django_comment_client.base.views.vote_for_comment': 0.0, 
+            'django_comment_client.base.views.update_comment': 0.0,
+            'django_comment_client.base.views.create_comment': 0.0,
+            'showanswer': 0.0,
+            'problem_save': 0.0,
+            'problem_check': 0.0,
+            'problem_graded': 0.0
+        }
     
+    def load_structure(self):
+        self.course_structure = dict()
+        self.course_mapping = dict()
+
+        filename = self.result_dir + self.c_id + '.course_structure'
+        file_handle = open(filename, 'r', encoding='utf-8')
+        try:
+            self.course_structure = json.loads(file_handle.readline(), strict=False)
+            self.course_mapping = json.loads(file_handle.readline(), strict=False)
+        except(ValueError, KeyError):
+            print ('error loading course structure')
+        file_handle.close()
+
+    def 
 def main():
     a = Analyzer('20740042X')
 
